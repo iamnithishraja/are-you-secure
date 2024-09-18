2443636b3566faa6e0657e40e6b5678d9c17749b
Your Name <you@example.com>
2024-09-18 14:11:26 +0530

    add password endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import Users from "../models/userSchema.js";
 import axios from "axios";
 import sendEmail from "../utils/sendEmail.js";
+import crypto from 'crypto'
 async function pushUser(req, res) {
   try {
     const userid = req.body.userId;
@@ -51,4 +52,43 @@ async function getBreachAnalysis(req, res) {
     res.json({ msg: "didnt get data" });
   }
 }
-export { checkBreach, pushUser, getBreachAnalysis };
+
+function keccakHash(password) {
+  const hash = crypto.createHash('sha3-512'); 
+  hash.update(password, 'utf8');
+  return hash.digest('hex').substring(0, 10); 
+}
+
+async function checkPassword(req,res) {
+  const password=req.params.password
+  if (password) {
+    const pwdHash = keccakHash(password);
+    const encodedPwdHash = encodeURIComponent(pwdHash); // URI encode the hash
+    const url = `https://passwords.xposedornot.com/api/v1/pass/anon/${encodedPwdHash}`;
+
+    try {
+      const response = await axios.get(url); 
+
+      if (response.status === 200) {
+        res.json(response.data);
+      } else if (response.status === 404) {
+        console.log('Password is safe');
+        res.json({"success":false});
+      } else {
+        console.error(`Error: ${response.status}`);
+        res.json({"success":false});
+      }
+    } catch (error) {
+      console.error(`Request failed: ${error.message}`);
+      res.json({"success":false});
+
+    }
+  } else {
+    console.log('Oops! Try again with a valid password.');
+    res.json({"success":"false"});
+  }
+}
+
+
+
+export { checkBreach, pushUser, getBreachAnalysis,checkPassword };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,13 @@ import {
   pushUser,
   checkBreach,
   getBreachAnalysis,
+  checkPassword,
 } from "../controllers/userController.js";
 
 const userRoutes = express.Router();
 userRoutes.post("/pushUser", pushUser);
 userRoutes.get("/checkBreach/:email", checkBreach);
 userRoutes.get("/getBreachAnalysis/:email", getBreachAnalysis);
+userRoutes.get('/checkPassword',checkPassword);
 
 export default userRoutes;
