3fa511c45211f411071da7b69d13c9377d02897e
bsc.lab01 bsc.lab01 <bsc.lab01@ad.christuniversity.in>
2024-09-18 12:23:51 +0530

    add banner displaying whether email is breached

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -3,21 +3,21 @@ import { Input } from "../components/ui/Input";
 import { ModeToggle } from "../components/ui/dark-mode-toggle";
 import { Button } from "../components/ui/button";
 import Navbar from "../components/navbar";
+import Banner from "../components/banner";
 
 function App() {
   return (
-    <div className="grid grid-cols-12 h-screen w-screen">
+    <div className=" h-screen w-screen">
       <Navbar />
-      <div className="col-start-4 flex flex-col justify-center h-full col-span-2">
-        <div className="border-2 py-6 px-4 rounded-lg flex flex-col gap-y-4">
-          <Input
-            className="w-48 border"
-            type="password"
-            placeholder="Password"
-          />
-          <Button variant="default" className="size-fit">
-            Submit
-          </Button>
+      <Banner />
+      <div className="grid grid-cols-12 h-full w-full">
+        <div className="col-start-3 flex flex-col h-full col-span-2 my-8">
+          <div className="border-2 py-6 px-4 rounded-lg flex flex-col gap-y-4">
+            <Input className="w-48 border" type="text" placeholder="Password" />
+            <Button variant="default" className="size-fit">
+              Submit
+            </Button>
+          </div>
         </div>
       </div>
     </div>
---
+++ frontend/src/components/banner.jsx
@@ -0,0 +1,46 @@
+import { useRecoilState } from "recoil";
+import { userAtom } from "../recoil/userAtoms";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Banner() {
+  const [banner, setBanner] = useRecoilState(userAtom);
+  const [isBreached, setIsBreached] = useState(false);
+
+  const url = "http://localhost:3000";
+
+  useEffect(() => {
+    const fetchBreachStatus = async () => {
+      if (banner?.email) {
+        try {
+          const res = await axios.get(
+            `http://${url}/users/checkBreach/${banner.email}`
+          );
+          setIsBreached(res.data.isBreached);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchBreachStatus();
+
+    const intervalId = setInterval(fetchBreachStatus, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [banner.email]);
+
+  return (
+    <div
+      className={`h-1/5 w-full flex items-center justify-center ${
+        isBreached ? "bg-red-600" : "bg-green-600"
+      }`}
+    >
+      <h1 className="text-white text-xl font-bold">
+        {banner.email ? banner.email : "No email found"}
+      </h1>
+    </div>
+  );
+}
+
+export default Banner;
